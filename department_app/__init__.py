4d4d34ca49cc0ee2a5ef5dd2cfeec435d24c3526
Stepan Maksymyk <smaksymy@gmail.com>
2022-01-11 12:43:13 +0200

    add some db connections

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -1,5 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Department(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey("department.id"))
+    date_of_birth = db.Column(db.Date, nullable=False)
+    salary = db.Column(db.Integer)
+
+    def __repr__(self):
+        return self.name
+
 
 import department_app.views
--- department_app/views.py
+++ department_app/views.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from department_app import app
+from flask import Flask, render_template, url_for, request, redirect
+from department_app import app, Department, db
 
 
 @app.route('/')
@@ -8,21 +8,57 @@ def index():
     return render_template("index.html", content='<h1>Main page<h1>')
 
 
-@app.route('/departments')
+@app.route('/departments', methods=['POST', 'GET'])
 def departments():
-    return render_template("departments.html", content='<h1>Main page<h1>')
+    if request.method == 'POST':
+        name = request.form['name']
+
+        department = Department(name=name)
+
+        try:
+            db.session.add(department)
+            db.session.commit()
+            return redirect('/departments')
+        except:
+            return "DB_ADD_ERROR"
+    else:
+        departments = Department.query.order_by(Department.id).all()
+        return render_template("departments.html", content='<h1>Main page<h1>', departments=departments)
 
 
-@app.route('/department/<int:id>')
+@app.route('/department/<int:id>', methods=['POST', 'GET'])
 def department(id):
-    return render_template("department.html", department_id=str(id))
+    if request.method == 'POST':
+        pass
+    else:
+        department = Department.query.get(id)
+        return render_template("department.html", department=department)
 
 
-@app.route('/employees')
+@app.route('/department/<int:id>/delete')
+def department_delete(id):
+        department = Department.query.get_or_404(id)
+
+        try:
+            db.session.delete(department)
+            db.session.commit()
+            return redirect('/departments')
+        except:
+            return "DB_DEL_ERROR"
+
+
+
+@app.route('/employees', methods=['POST', 'GET'])
 def employees():
-    return render_template("employees.html", content='<h1>Main page<h1>')
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template("employees.html", content='<h1>Main page<h1>')
 
 
-@app.route('/employee/<int:id>')
+@app.route('/employee/<int:id>', methods=['POST', 'GET'])
 def employee(id):
-    return render_template("employee.html", employee_id=str(id))
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template("employee.html", employee_id=str(id))
